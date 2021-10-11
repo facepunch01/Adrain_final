10767b34f64a03f492e61af2d645e55ec6ed5922
thatboyjake <45983348+thatboyjake@users.noreply.github.com>
2021-10-11 10:34:20 -0500

    Added cms

---
+++ svelte/src/lib/hero.svelte
@@ -0,0 +1,14 @@
+<script>
+    export let text;
+</script>
+<div>
+    <h1 class="text">{text}</h1>
+    <slot class="Content"></slot>
+</div>
+<style lang="scss">
+    @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap');
+    .text {
+        font-size: 15em;
+        font-family: "Montserrat";
+    }
+</style>
