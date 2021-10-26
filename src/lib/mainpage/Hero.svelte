5b73c89d4563996dabcc59bf4b8649a96a4a5459
thatboyjake <45983348+thatboyjake@users.noreply.github.com>
2021-10-26 11:48:02 -0500

    the test

--- src/lib/mainpage/Hero.svelte
+++ src/lib/mainpage/Hero.svelte
@@ -7,7 +7,7 @@
         <h1 class="center-grid col-start-1 text-4xl px-14 bg-[#FFD8A7] md:text-6xl lg:text-7xl text-[#0563C1] col-end-3 grid-item">Your Friendly neighborhood Bugle</h1>
         <p class="center-grid text-[#05B1C1] text-4xl bg-[#333] md:h-[81%] lg:text-7xl col-start-1 h-44 col-end-2 row-start-2 row-end-3 grid-item uppercase"><a href="#Blog">blog</a></p>
         <p class="center-grid text-[#333] text-4xl bg-[#05B1C1] md:h-[81%] lg:text-7xl col-start-2 h-44 col-end-3 row-start-2 row-end-3 grid-item uppercase"><a href="#Go">go</a></p>
-        <img src="./Hero-img.jpg" class="hidden sm:block h-[86.7%] md:h-[90.5%] w-[100%] object-cover col-start-3 col-end-4 row-start-1 row-end-3" alt="">
+        <img src="../static/hero-img.jpg" class="hidden sm:block h-[86.7%] md:h-[90.5%] w-[100%] object-cover col-start-3 col-end-4 row-start-1 row-end-3" alt="">
     </div>
 </section>
 
